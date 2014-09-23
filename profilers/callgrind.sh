0e16542cf7623af0b42587822206e805e4d0f1dd
David Froger <david.froger@inria.fr>
2014-09-23 15:01:35 +0200

    Improve callgrind script

--- profilers/callgrind.sh
+++ profilers/callgrind.sh
@@ -1,15 +1,17 @@
 source common.sh
 
-g++ -O2 -c src/main.cxx
-g++ -O2 -c src/nbody.cxx
-g++ -O2 -o profme main.o nbody.o
+#g++ -O2 -c src/main.cxx
+#g++ -O2 -c src/nbody.cxx
+#g++ -O2 -o profme main.o nbody.o
 
-valgrind --tool=callgrind ./profme $NPARTICULES $NTIMES
+#valgrind --tool=callgrind ./profme $NPARTICULES $NTIMES
 
-callgrind_annotate
+callgrind_annotate --tree=both > callgrind.txt
 
 mv profme callgrind.out.* callgrind.txt $RESULT_DIR
 rm *.o
 
 echo
 echo "See files in $RESULT_DIR/"
+echo "Run: kcachegrind $RESULT_DIR/callgrind.out.*"
+echo "Note: Don't know how to generate in ascii files same information as kcachedgrind"
