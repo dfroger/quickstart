6fcb9e439e3df850685e2938c186a632ecdbeb91
David Froger <david.froger@inria.fr>
2014-12-07 14:23:32 +0100

    mpi: print rank

--- mpi4py-compute-pi/main.c
+++ mpi4py-compute-pi/main.c
@@ -6,18 +6,20 @@
 
 int main( int argc, char *argv[] ) 
 { 
-    int mpi_rank, mpi_size; 
+    int rank, size; 
     double pi_ref = 3.141592653589793238462643; 
     double pi; 
     MPI_Init(&argc,&argv); 
-    MPI_Comm_size(MPI_COMM_WORLD,&mpi_size);
-    MPI_Comm_rank(MPI_COMM_WORLD,&mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+    printf("rank %d / %d\n", rank, size);
 
     int n = 100;
-    double mypi = compute_pi(mpi_rank, mpi_size, n, MPI_COMM_WORLD);
+    double mypi = compute_pi(rank, size, n, MPI_COMM_WORLD);
     MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); 
 
-    if (mpi_rank == 0)
+    if (rank == 0)
         printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - pi_ref)); 
 
     MPI_Finalize(); 
