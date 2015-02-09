504d4ba34cee5ca79466dcb009d630dc28e81e13
David Froger <david.froger@inria.fr>
2015-02-09 21:35:06 +0100

    ITK iterators

---
+++ itk/iterators/plot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import matplotlib
+matplotlib.use('Agg')
+from pylab import *
+
+
+c_pointer_size = []
+c_pointer_seconds = []
+
+itk_iterators_size = []
+itk_iterators_seconds = []
+
+with open('times.txt') as f:
+    for line in f.readlines():
+        method, ny, nx, seconds = line.split()
+        ny = int(ny)
+        nx = int(nx)
+        seconds = float(seconds)
+        if method == 'c_pointer':
+            c_pointer_size.append(nx*ny)
+            c_pointer_seconds.append(seconds)
+        elif method == 'itk_iterator':
+            itk_iterators_size.append(nx*ny)
+            itk_iterators_seconds.append(seconds)
+        else:
+            raise ValueError, "No such method: %r" % method
+
+assert c_pointer_size == itk_iterators_size
+
+print c_pointer_size, c_pointer_seconds
+print itk_iterators_size, itk_iterators_seconds
+
+plot(c_pointer_size, c_pointer_seconds, 'r-', label='C pointers')
+plot(itk_iterators_size, itk_iterators_seconds, 'b-', label='ITK iterators')
+legend()
+savefig('times.png')
---
+++ itk/iterators/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NITER=100
+VERBOSE=0
+
+rm -f times.txt
+
+for N in $(seq 100 100 1000)
+do
+    echo $N
+    ./iterators $N $N $NITER $VERBOSE >> times.txt
+done
