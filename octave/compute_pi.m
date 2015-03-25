116e1eca4f630b6fd24193d3e537016710ec5752
David Froger <david.froger@inria.fr>
2015-03-25 18:35:48 +0100

    octave: add function

--- octave/compute_pi.m
+++ octave/compute_pi.m
@@ -1,18 +1,19 @@
-n = 200;
-h = 1.0 / n;
-printf('%d\n', n);
-printf('%g\n', h);
+function[mypi] = compute_pi(mpi_rank, mpi_size, n)
+% document here
+
+    h = 1.0 / n;
+    sum = 0.;
+
+    for (i = mpi_rank + 1 : mpi_size : n)
+        x = h * (i - 0.5);
+        sum = sum + (4.0 / (1.0 + x*x)); 
+    end
+
+    mypi = h * sum;
+end
 
 mpi_rank = 0;
 mpi_size = 1;
+n = 200;
 
-sum = 0.;
-
-for (i = mpi_rank + 1 : mpi_size : n)
-    x = h * (i - 0.5);
-    sum = sum + (4.0 / (1.0 + x*x)); 
-end
-
-result = h * sum
-
-printf('%15.10g\n', result);
+compute_pi(mpi_rank, mpi_size, n)
