e25c9986137ae30183145c92c50306b02027360c
David Froger <david.froger@inria.fr>
2015-07-08 14:24:27 +0200

    Add reference links

--- c_int/main.c
+++ c_int/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// https://en.wikipedia.org/wiki/C_data_types
+// http://stackoverflow.com/a/13604190
+// http://en.cppreference.com/w/cpp/types/integer
+
 #define PRINT_SIZE_OF(type) printf("%10s: %lu byte(s)\n", #type, sizeof(type));
 
 #ifndef _GKLIB_H_
@@ -17,6 +21,7 @@ int main()
     PRINT_SIZE_OF(char);
     PRINT_SIZE_OF(short);
     PRINT_SIZE_OF(int);
+    PRINT_SIZE_OF(unsigned long);
     PRINT_SIZE_OF(long);
     PRINT_SIZE_OF(long long);
     PRINT_SIZE_OF(int32_t);
