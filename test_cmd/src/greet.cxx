1c142eaaca6ee43d6787cf49425b7c8913839003
David Froger <david.froger@inria.fr>
2015-07-20 14:21:40 +0200

    Start waf project example

---
+++ test_cmd/src/greet.cxx
@@ -0,0 +1,8 @@
+#include "greet.hxx"
+
+using namespace std;
+
+string greet(string name)
+{
+    return "Hello, " + name + "!";
+}
---
+++ test_cmd/src/greet.hxx
@@ -0,0 +1,8 @@
+#ifndef GREET_HXX_INCLUDED
+#define GREET_HXX_INCLUDED
+
+#include <string>
+
+std::string greet(std::string name);
+
+#endif
