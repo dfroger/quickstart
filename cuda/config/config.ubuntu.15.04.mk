114a165c5bc563c2a9f0df8f855e0d4feb424f6c
David Froger <david.froger@inria.fr>
2016-02-05 08:31:13 +0100

    Example works on Ubuntu 15.04

--- cuda/config/config.ubuntu.15.04.mk
+++
@@ -1,9 +0,0 @@
-# https://doc.ubuntu-fr.org/cuda
-# https://developer.nvidia.com/cuda-downloads
-# http://docs.nvidia.com/cuda/cuda-getting-started-guide-for-linux/index.html
-# wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1504-7-5-local_7.5-18_amd64.deb
-# sudo dpkg -i cuda-repo-ubuntu1504-7-5-local_7.5-18_amd64.deb
-# sudo apt-get update
-# sudo apt-get install cuda
-# export PATH=/usr/local/cuda-7.0/bin:$PATH
-$ export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
