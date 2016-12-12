10cc6ee2b4a4b59152f5f8501fbe9425fe3a5faf
David Froger <david.froger@mailoo.org>
2016-12-12 08:39:07 +0100

    Bash snippets

--- bash/snippets.sh
+++ bash/snippets.sh
@@ -15,3 +15,13 @@ fi
 
 # http://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# https://www.cyberciti.biz/faq/bash-infinite-loop/
+while true
+do
+    echo "Press [CTRL+C] to stop.."
+    sleep 1
+done
+
+# Get timestamp
+timestamp=$(date +%s)
