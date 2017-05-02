092974508cf96d40519c8f838030a90580928af4
David Froger <david.froger@mailoo.org>
2017-05-02 15:29:41 +0200

    cosmetics

--- monetdb/run_test.py
+++ monetdb/run_test.py
@@ -27,18 +27,23 @@ mdb_daemon('set', 'passphrase={}'.format(passphrase), farm)
 mdb_daemon('start', farm)
 mdb_daemon('get', 'all', farm)
 
-# Create a `demo` database
-ctl = pymonetdb.control.Control(port=port, passphrase=password)
+# Create a remote control. Databases are not manage with the 'monetdb create',
+# 'monetdb start', ...  command line tools like in run_test.sh, but instead
+# use the remote control.
+ctl = pymonetdb.control.Control(port=port, passphrase=passphrase)
+
+# Create a `demo` database.
 ctl.create(db)
 ctl.start(db)
 ctl.get(db)
 
-# Create a table
+# Get connection cursor
 connection = pymonetdb.connect(username=user, password=password,
                                hostname=hostname, database=db,
                                port=port)
 cursor = connection.cursor()
 
+# Create a table
 cursor.execute("CREATE TABLE {} (id integer,name varchar(20));".format(table))
 
 # Add rows to table
@@ -48,9 +53,10 @@ cursor.execute("INSERT INTO {} VALUES (1, 'Bob');".format(table))
 # Get values
 cursor.execute("SELECT * FROM {};".format(table))
 cursor.fetchall()
-cursor.close()
 
-# Stop farm
+# Clean everything
+cursor.close()
+connection.close()
 ctl.stop(db)
 ctl.destroy(db)
 mdb_daemon('stop', farm)
